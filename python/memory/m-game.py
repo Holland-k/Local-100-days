3edeaffaa4b0284d282fa0127a05570f4ac264d5
Holland <holland@hollandk.com>
2018-06-20 19:05:52 -0400

    memory game, designing board

--- python/memory/m-game.py
+++ python/memory/m-game.py
@@ -1,2 +1,18 @@
-print("hello world")
-print("2")
+game = 1
+
+def print_board():
+	pass
+
+while(game):
+	print_board()
+	print("Choose two cards (a1 b1)")
+	
+	try:
+		n = str(raw_input())
+		if(i = n.index(' ')):
+			in1 = n[:i]
+			in2 = n[i+1:]
+			print(in1)
+			print(in2)
+	except ValueError:
+		print("Please enter a number.\n")
