e5980cb032057fe086eea1aa161514b784789d1f
Holland <holland@hollandk.com>
2018-06-24 18:35:01 -0400

    exercises from Programming in Haskell

---
+++ haskell/hs_ex.hs
@@ -0,0 +1,64 @@
+-- A.2
+
+-- problem 3
+naa = a `div` length xs 
+    where 
+        a = 10
+        xs = [1,2,3,4,5]
+
+-- problem 4
+my_last = head(reverse([1..5]))
+
+-- problem 5
+
+my_init_1 = reverse(tail(reverse([1..5])))
+
+my_init_2 = take (length([1..5]) - 1) [1..5]
+
+-- A.4
+
+even :: Integral a => a -> Bool
+even n = n `mod` 2 == 0
+
+odds n = map (\x -> x*2 + 1) [0..n-1]
+const x = \_ -> x
+
+my_fun (#) = \x -> (\y -> x # y)
+
+-- problem 1
+halve x = (take n x, drop n x)
+    where n = length x `div` 2
+
+-- problem 2
+third1 xs =  head(tail(tail(xs)))
+
+third2 xs = xs !! 2
+
+third3 (_:_:x:xs) = x
+
+-- problem 3
+
+safetail1 xs = if null xs then [] else tail xs
+
+safetail2 xs | null xs     = []
+    | otherwise             = tail xs
+
+
+safetail3 []        = []
+safetail3 (_:xs)    = xs 
+
+-- Exercises 5.7
+
+-- problem 1
+
+sumsq n = sum [x^2 | x <- [1..n]]
+
+-- problem 2
+
+grid :: Int -> Int -> [(Int,Int)]
+grid x y = [(n,m) | n <- [0..x], m <- [0..y]]
+
+-- problem 3
+
+square :: Int -> [(Int,Int)]
+square a = [(b,c) | (b,c) <- grid a a, b /= c]
