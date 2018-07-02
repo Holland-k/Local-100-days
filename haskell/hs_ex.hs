ba0bf7e788fdacd3ccaf4c2f78c325445cc46fa6
Holland <holland@hollandk.com>
2018-07-02 16:14:55 -0400

    tautology checker

--- haskell/hs_ex.hs
+++ haskell/hs_ex.hs
@@ -150,3 +150,48 @@ luhn :: [Int] -> Bool
 luhn x = if y `mod` 10 == 0 then True else False
     where y = luhnSum (luhnCalc x)
 
+-- Chapter 8
+data Prop = Const Bool
+    | Var Char
+    | Not Prop
+    | And Prop Prop
+    | Imply Prop Prop
+
+type Assoc k v = [(k,v)]
+
+type Subst = Assoc Char Bool
+
+p2 :: Prop
+p2 = Imply (And (Var 'A') (Var 'B')) (Var 'A')
+
+rmdups :: Eq a => [a] -> [a]
+rmdups [] = []
+rmdups (x:xs) = x : filter (/= x) (rmdups xs)
+
+bools :: Int -> [[Bool]]
+bools 0 = [[]]
+bools n = map (False:) (bools (n-1)) ++ map (True:) (bools (n-1))
+
+vars :: Prop -> [Char]
+vars (Const _) = []
+vars (Var x) = [x]
+vars (Not p) = vars p
+vars (And p q) = (vars p) ++ (vars q)
+vars (Imply p q) = vars p ++ vars q
+
+substs :: Prop -> [Subst]
+substs p = map (zip vrs) (bools (length vrs))
+    where vrs = rmdups (vars p)
+
+find :: Eq k => k -> Assoc k v -> v
+find k t = head [v | (k',v) <- t, k == k']
+
+eval :: Subst -> Prop -> Bool
+eval _ (Const b) = b
+eval s (Var x) = find x s
+eval s (Not p) = not (eval s p)
+eval s (And p q) = (eval s p) && (eval s q)
+eval s (Imply p q) = (eval s p) <= (eval s q)
+
+isTaut :: Prop -> Bool
+isTaut p = and [eval s p | s <- substs p]
