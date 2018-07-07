25d86c08dcfce3cf8f35faf811a24696c8454648
Holland <holland@hollandk.com>
2018-07-07 10:36:25 -0400

    finished the golf problem histogram

--- haskell/golf.hs
+++ haskell/golf.hs
@@ -1,3 +1,4 @@
+import Data.List
 -- doesn't work properly
 skips :: [a] -> [[a]]
 skips [] = [[]]
@@ -7,3 +8,15 @@ localMaxima :: [Integer] -> [Integer]
 localMaxima (a:b:c:xs) = if (b > a) && (b > c) then b:y else y
 	where y = localMaxima(b:c:xs)
 localMaxima _ = []
+
+histGraph :: Int -> String
+histGraph n = replicate n '*'
+
+printClean n = (intercalate "\n" (reverse (transpose (map (++ q) n)))) 
+    ++ "\n" 
+    ++ replicate 10 '=' ++ "\n"
+    ++ "0123456789\n" 
+    where q = replicate (length (maximum n)) ' '
+
+histogram :: [Integer] -> String
+histogram a = printClean [histGraph (length (filter (== n) a)) | n <- [0..9]]
