ecd7966f5c3fee0084ab65bd5de484df61e7a9b2
Holland <holland@hollandk.com>
2018-07-12 14:18:47 -0400

    more work on week 5

--- haskell/calc.hs
+++ haskell/calc.hs
@@ -13,6 +13,29 @@ instance Expr ExprT where
     add = Add 
     mul = Mul
 
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Integer where 
+    lit = id
+    add = (+)
+    mul = (*)    
+
+instance Expr Bool where 
+    lit i = if i > 0 then True else False
+    add = (||)
+    mul = (&&)
+
+instance Expr MinMax where 
+    lit = MinMax
+    add (MinMax a) (MinMax b) = (MinMax (max a b))
+    mul (MinMax a) (MinMax b) = (MinMax (min a b))
+
+instance Expr Mod7 where
+    lit a = (Mod7 (a `mod` 7))
+    add (Mod7 a) (Mod7 b) = Mod7 ((a + b) `mod` 7)
+    mul (Mod7 a) (Mod7 b) = (Mod7 ((a * b) `mod` 7))
+
 eval :: ExprT -> Integer
 eval (Lit t) = t
 eval (Add x y) = (eval x) + (eval y)
@@ -25,3 +48,6 @@ evalStr x = case parseExp Lit Add Mul x of
 
 reify :: ExprT -> ExprT
 reify = id
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul "(3 * -4) + 5"
