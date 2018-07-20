91ffb514a87dcd73696ad084943b62c825159beb
Holland <holland@hollandk.com>
2018-07-20 11:33:39 -0400

    revised solution based on feedback

--- exercism/haskell/nucleotide-count/src/DNA.hs
+++ exercism/haskell/nucleotide-count/src/DNA.hs
@@ -1,12 +1,21 @@
 module DNA (nucleotideCounts) where
 
 import qualified Data.Map as Map
-import Data.List
+
+data Nucleotide = A | C | G | T deriving (Show, Eq)
+
+toNucleotide :: Char -> Maybe Nucleotide
+toNucleotide x 
+    | x == 'A' = Just A
+    | x == 'C' = Just C
+    | x == 'G' = Just G
+    | x == 'T' = Just T
+    | otherwise = Nothing
 
 ntCount :: String -> Char -> (Char, Int)
 ntCount xs a = (a, sum $ map (\x -> if a == x then 1 else 0) xs)
 
 nucleotideCounts :: String -> Either String (Map.Map Char Int)
 nucleotideCounts xs 
-    | ((not . and $ map (\x-> elem x "AGCT") xs) && ((length xs) /= 0)) = Left "Bad String"
-    | otherwise = Right (Map.fromList (map (ntCount xs) "ACTG"))
+    | ((sequenceA $ fmap toNucleotide xs) == Nothing) && length xs /= 0 = Left "Bad String"
+    | otherwise = Right (Map.fromList (map (ntCount xs) "ACTG")) -- still have to return (Map Char Int)
